52f7d4658c383f0d7fccc903b077d8b9ea5233d3
speedingdeer <ffwisniewski@gmail.com>
2016-01-12 21:10:22 +0100

    Deleted old 2015 directory.

--- 2015/nightwatch/globals.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    domain: "http://127.0.0.1:9000/"
-};
--- 2015/nightwatch/tests/landing/title.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'Check Title': function(browser) {
-    browser
-      .url(browser.globals.domain)
-      .assert.containsText("h1", "nucl.ai", "It's nucl.ai!")
-      .end();
-  }
-};
