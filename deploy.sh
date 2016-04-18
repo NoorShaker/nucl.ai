3d1319eef107d7550861afb0fdc17b3dc9cf9aeb
speedingdeer <ffwisniewski@gmail.com>
2016-04-18 11:14:57 +0200

    Fixed deploy recipe.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-grunt clean; git checkout gh-pages; git pull -u; git merge -X theirs master; grunt build; git commit -a; git push; git checkout master
+grunt clean; git checkout gh-pages; git pull -u; git merge -X theirs master; grunt build; git commit -a; git push origin gh-pages --force; git checkout master
